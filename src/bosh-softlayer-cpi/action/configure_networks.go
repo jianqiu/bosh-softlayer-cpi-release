9b812497943486339ff224ffd10e71cba3950345
jianqiu <wangjq@cn.ibm.com>
2015-12-14 17:20:06 +0800

    Changed bosh-softlayer-cpi as subfolder of bosh-softlayer-cpi-release project

---
+++ src/bosh-softlayer-cpi/action/configure_networks.go
@@ -0,0 +1,33 @@
+package action
+
+import (
+	bosherr "github.com/cloudfoundry/bosh-utils/errors"
+
+	bslcvm "github.com/maximilien/bosh-softlayer-cpi/softlayer/vm"
+)
+
+type ConfigureNetworks struct {
+	vmFinder bslcvm.Finder
+}
+
+func NewConfigureNetworks(vmFinder bslcvm.Finder) ConfigureNetworks {
+	return ConfigureNetworks{
+		vmFinder: vmFinder,
+	}
+}
+
+func (a ConfigureNetworks) Run(vmCID VMCID, networks bslcvm.Networks) (interface{}, error) {
+	vm, found, err := a.vmFinder.Find(int(vmCID))
+	if err != nil {
+		return nil, bosherr.WrapErrorf(err, "Finding vm '%s'", vmCID)
+	}
+
+	if found {
+		err := vm.ConfigureNetworks(networks)
+		if err != nil {
+			return nil, bosherr.WrapErrorf(err, "Configuring networks vm '%s'", vmCID)
+		}
+	}
+
+	return nil, nil
+}
